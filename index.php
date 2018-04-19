5a2cbb780cacce21ce3abb265394f74100ab62ba
stormy2021 <brian.m.horning@gmail.com>
2018-04-19 16:55:45 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+
+<?PHP echo "<h2>Intact Web Uploader</h2>"; ?>
+	<form action="splunk-results.php" id="myForm" name="frmupload" method="post" enctype="multipart/form-data">
+	<?php echo "<p>Welcome, select a file to upload.</p>\n"; ?>
+		<input type="file" name="fileToUpload" id="fileToUpload" multiple="multiple">
+		<br /><br />
+		<input type="submit" value="Upload File" name="submit" onclick='upload_image();'> 
+	</form>
+
+</body>
+</html>
